c248029b8bf6c1a22b9246f1f0bdf1d2717ffa7c
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-04-02 21:42:46 -0600

    editing app and making a header display

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import { Component } from 'react';
 
+import Header from './header';
+import List from './list';
+
 export default class App extends Component {
-  render() {
-    return (
-      <div className="text-center">
-        <h1>My Todo List</h1>
-      </div>
-    );
-  }
+ constructor(props) {
+   super(props)
+   this.state = {
+     items: []
+   }
+
+   this.onAddClick = this.onAddClick.bind(this);
+ }
+
+ onAddClick(newItem) {
+   this.setState({
+     items: this.state.items.concat([newItem])
+   });
+ }
+
+ render() {
+   return (
+     <div className="text-center">
+       <h1>My Todo List</h1>
+       <Header add={this.onAddClick} />
+       <List items={this.state.items}/>
+     </div>
+   );
+ }
 }
